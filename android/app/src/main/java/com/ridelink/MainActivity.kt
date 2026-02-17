5b5041ed1f47b43454dc7abab229fde959db4788
KinzaKhurram123 <kinzakhurram84@gmail.com>
2026-02-17 21:56:38 +0500

    changes

--- android/app/src/main/java/com/ridelink/MainActivity.kt
+++ android/app/src/main/java/com/ridelink/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ridelink
+package com.C2
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/ridelink/MainApplication.kt
+++ android/app/src/main/java/com/ridelink/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ridelink
+package com.C2
 
 import android.app.Application
 import com.facebook.react.PackageList
